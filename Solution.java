d88272b174f2ea6df790cfcc4adcd579e9c52f57
Vinci <1341060890@qq.com>
2024-06-01 11:47:25 +0800

    141

---
+++ Solution.java
@@ -0,0 +1,44 @@
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        // // 采用集合的方法，复杂度太高
+        // Set<ListNode> set = new HashSet<>();
+
+        // ListNode curr = head;
+        // while(curr!=null){
+        //     if(set.contains(curr)){
+        //         return true;
+        //     }
+        //     set.add(curr);
+        //     curr=curr.next;
+        // }
+        // return false;
+
+        // 双指针法
+
+        ListNode fast = head;
+        ListNode slow = head;
+
+
+        while(fast!=null &&fast.next!=null){
+            
+            slow=slow.next;
+            fast=fast.next.next;
+            if(fast==slow){
+                return true;
+            }
+        }
+        return false;
+        
+    }
+}
