d284c1428f2079c57e9b04ac7f22a6e2094e3203
Vinci <1341060890@qq.com>
2024-06-06 20:19:45 +0800

    2824

---
+++ 2824.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int countPairs(List<Integer> nums, int target) {
+        Collections.sort(nums);
+        int ans = 0;
+        int left = 0;
+        int right = nums.size()-1;
+        while(left<right){
+            if(nums.get(left)+nums.get(right)<target){
+                ans+=right-left;
+                left++;
+            }
+            else{
+                right--;
+            }
+        }
+        return ans;
+    }
+}
