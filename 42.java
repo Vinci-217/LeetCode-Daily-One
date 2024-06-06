d2b440d3daabbb3634a419776a3b6f8b64af5473
Vinci <1341060890@qq.com>
2024-06-06 11:30:58 +0800

    42（非常通透的接雨水）

--- 42.java
+++ 42.java
@@ -1,36 +1,44 @@
 class Solution {
     public int trap(int[] height) {
-        int sum = 0;
-        int max_left = 0;
-        int max_right = 0;
 
-        int left=1;
-        int right = height.length-2;
+        // 方法一：先左右遍历，分别求出前缀最大和后缀最大，然后从左向右遍历
 
+        // int[] pre_max = new int[height.length];
+        // pre_max[0] = height[0];
+        // for(int i=1;i<height.length;i++){
+        //     pre_max[i] = Math.max(height[i],pre_max[i-1]);
+        // }
 
+        // int[] suf_max = new int[height.length];
+        // suf_max[height.length-1]=height[height.length-1];
+        // for(int i=height.length-2;i>=0;i--){
+        //     suf_max[i] = Math.max(height[i],suf_max[i+1]);
+        // }
+        // int res = 0;
+        // for(int i=0;i<height.length;i++){
+        //     res+=Math.min(suf_max[i],pre_max[i])-height[i];
+        // }
+        // return res;
 
-        for (int i = 1; i < height.length - 1; i++) {
-
-            if(height[left-1]-1<height[right+1]){
-                max_left=Math.max(max_left,height[left-1]);
-                int min =max_left;
-                if(min>height[left]){
-                    sum += min - height[left];
-                }
+        // 方法二：如果前缀最大小于后缀最大，那么根据木桶效应，最多也就是前缀最大了，反之同理。然后从左右向中间扩展
+        int n = height.length;
+        int left = 0;
+        int right = n-1;
+        int res = 0;
+        int pre_max = height[0];
+        int suf_max = height[n-1];
+        while(left<=right){
+            pre_max = Math.max(height[left],pre_max);
+            suf_max = Math.max(height[right],suf_max);
+            if(pre_max<suf_max){
+                res+=pre_max-height[left];
                 left++;
             }
             else{
-                max_right=Math.max(max_right,height[right+1]);
-                int min =max_right;
-                if(min>height[right]){
-                    sum += min - height[right];
-                }
+                res+=suf_max-height[right];
                 right--;
             }
-
         }
-
-        return sum;
-
+        return res;
     }
 }
