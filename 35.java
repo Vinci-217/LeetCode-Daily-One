87235dcb3b0e52d101d956c19dc10a080ee217d1
Vinci <1341060890@qq.com>
2024-06-19 11:08:31 +0800

    35

---
+++ 35.java
@@ -0,0 +1,18 @@
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = -1;
+        int right = nums.length;
+        while (left + 1 < right) {
+            int mid = left + (right - left) / 2;
+            if (nums[mid] >= target) {
+                right = mid;
+            } else {
+                left = mid;
+            }
+        }
+        return right;
+
+    }
+
+
+}
