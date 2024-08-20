0c8230338faa0cc3e16a8f6ae49e4c5d79ed0267
Vinci <1341060890@qq.com>
2024-08-20 17:03:11 +0800

    257（使用新方法

--- 257.java
+++ 257.java
@@ -35,3 +35,55 @@ class Solution {
         dfs(node.right,new StringBuilder(str.toString()));
     }
 }
+
+
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    List<String> ans = new ArrayList<>();
+
+    public List<String> binaryTreePaths(TreeNode root) {
+        if(root == null) return ans;
+        StringBuilder path = new StringBuilder();
+        dfs(root,path);
+        return ans;
+    }
+
+    private void dfs(TreeNode node,StringBuilder path){
+
+        path.append(node.val);
+        if(node.left!=null){
+            path.append("->");
+            dfs(node.left,new StringBuilder(path.toString()));
+            path.delete(path.length()-2,path.length());
+        }
+        if(node.right!=null){
+            path.append("->");
+            dfs(node.right,new StringBuilder(path.toString()));
+            path.delete(path.length()-2,path.length());
+        }
+        if(node.left==null&&node.right==null){
+            ans.add(path.toString());
+//            path.delete(path.length()-3,path.length());
+            return;
+        }
+
+        return;
+
+
+    }
+}
