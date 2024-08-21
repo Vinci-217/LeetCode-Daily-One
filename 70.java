c4cb2408ab5c784ac3bf7483f8c4086b52384f21
Vinci <1341060890@qq.com>
2024-08-21 14:35:35 +0800

    70（递归

--- 70.java
+++ 70.java
@@ -9,3 +9,23 @@ class Solution {
         return dp[n];
     }
 }
+
+// class Solution {
+//     private int[] memo;
+//     public int climbStairs(int n) {
+//         memo = new int[n+1];
+//         Arrays.fill(memo,-1);
+//         return dfs(n);
+//     }
+
+//     private int dfs(int i){
+//         if(i<=1){
+//             return 1;
+//         }
+//         if(memo[i]!=-1){
+//             return memo[i];
+//         }
+
+//         return memo[i]=dfs(i-1)+dfs(i-2);
+//     }
+// }
