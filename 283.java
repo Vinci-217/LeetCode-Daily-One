c403d5c920de46199c94fd28b1766290d6f642cc
Vinci-217 <1341060890@qq.com>
2024-08-27 11:12:49 +0800

    refactor: 283

--- 283.java
+++ 283.java
@@ -28,3 +28,57 @@ class Solution {
 
     }
 }
+
+// 第二次自己做的版本
+class Solution {
+    public void moveZeroes(int[] nums) {
+        int left = -1;
+
+        // O(n)
+        for(int i = 0;i<nums.length;i++){
+            if(nums[i]==0){
+                left = i;
+                break;
+            }
+
+        }
+        if(left==-1)
+            return;
+
+        int right = left+1;
+        if(right>=nums.length)
+            return;
+        while(nums[right]==0){
+            right++;
+            if(right>=nums.length)
+                break;
+
+        }
+        while (right<nums.length){
+            if(nums[right]!=0){
+                swap(nums,left,right);
+                left++;
+                while(nums[left]!=0)
+                    left++;
+                right=left+1;
+                if(right>=nums.length)
+                    break;
+                while(nums[right]==0){
+                    right++;
+                    if(right>=nums.length)
+                        break;
+
+                }
+
+            }
+        }
+
+    }
+
+    private void swap(int[] nums, int left ,int right){
+        int temp = nums[left];
+        nums[left] = nums[right];
+        nums[right] = temp;
+    }
+}
+
