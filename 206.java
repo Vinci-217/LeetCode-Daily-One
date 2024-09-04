f97f42d3c78b95b5791553f0865d668c820b80b2
Vinci <1341060890@qq.com>
2024-09-04 16:24:31 +0800

    206

--- 206.java
+++ 206.java
@@ -31,3 +31,29 @@ class Solution {
 
     }
 }
+
+// 灵神的版本，本质在于交换
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode reverseList(ListNode head) {
+        ListNode pre = null;
+        ListNode cur = head;
+        while(cur!=null){
+            // 提前把下一个节点存好了
+            ListNode next = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = next;
+        }
+        return pre;
+    }
+}
