e41d3c6db996509ab16d32e382d23c715df1e936
Vinci <1341060890@qq.com>
2024-09-09 20:25:09 +0800

    143

---
+++ 143.java
@@ -0,0 +1,48 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
+        ListNode mid = middleNode(head);
+        ListNode head2 = reverseList(mid);
+        while(head2.next!=null){
+            ListNode nxt = head.next;
+            ListNode nxt2 = head2.next;
+            head.next = head2;
+            head2.next = nxt;
+            head = nxt;
+            head2 = nxt2;
+        }
+
+    }
+
+        // 876. 链表的中间结点
+    private ListNode middleNode(ListNode head) {
+        ListNode slow = head, fast = head;
+        while (fast != null && fast.next != null) {
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+    // 206. 反转链表
+    private ListNode reverseList(ListNode head) {
+        ListNode pre = null, cur = head;
+        while (cur != null) {
+            ListNode nxt = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = nxt;
+        }
+        return pre;
+    }
+
+}
