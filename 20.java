465ba152c45f6a544cc1cf19c821f8badcc3ae11
Vinci-217 <1341060890@qq.com>
2024-09-25 11:14:28 +0800

    refactor: 20

--- 20.java
+++ 20.java
@@ -27,3 +27,28 @@ class Solution {
 
     }
 }
+// 第二次做法
+class Solution {
+    public boolean isValid(String s) {
+        Stack<Character> stack = new Stack<>();
+        for(int i = 0;i<s.length();i++){
+            if(s.charAt(i)=='('||s.charAt(i)=='{'||s.charAt(i)=='[')
+                stack.push(s.charAt(i));
+            else{
+                if(stack.isEmpty())
+                    return false;
+                Character ch = stack.peek();
+                if(s.charAt(i)-ch==1||s.charAt(i)-ch==2){
+                    stack.pop();
+                }
+                else{
+                    return false;
+                }
+            }
+        }
+        if(!stack.isEmpty())
+            return false;
+        return true;
+
+    }
+}
