05e0aa68fb748fd15c8c63e056e1a7b3e8a8184f
Vinci-217 <1341060890@qq.com>
2024-09-25 11:49:42 +0800

    feat: 415

---
+++ 415.java
@@ -0,0 +1,20 @@
+class Solution {
+    public String addStrings(String num1, String num2) {
+        StringBuilder sb = new StringBuilder();
+        int i = num1.length()-1;
+        int j = num2.length()-1;
+        int carry = 0;
+        while(i>=0||j>=0){
+            int n1 = i>=0?num1.charAt(i)-'0':0;
+            int n2 = j>=0?num2.charAt(j)-'0':0;
+            int tmp = n1+n2+carry;
+            carry = tmp/10;
+            sb.append(tmp%10);
+            i--;
+            j--;
+        }
+        if(carry==1)
+            sb.append(1);
+        return sb.reverse().toString();
+    }
+}
