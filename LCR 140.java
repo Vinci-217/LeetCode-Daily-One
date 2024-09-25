17cae4003843abdef5b78d71a43fbe9ac0bd7dcf
Vinci-217 <1341060890@qq.com>
2024-09-25 20:18:17 +0800

    feat: LCR 140

---
+++ LCR 140.java
@@ -0,0 +1,25 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public ListNode trainingPlan(ListNode head, int cnt) {
+        ListNode dummy = new ListNode(0,head);
+        ListNode slow = dummy;
+        ListNode fast = dummy;
+        for(int i=0;i<cnt;i++){
+            fast = fast.next;
+        }
+        while(fast!=null){
+            slow = slow.next;
+            fast = fast.next;
+        }
+        return slow;
+    }
+}
