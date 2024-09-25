1c69c9f4d5dbc2b581dab93cb5a1254e8716288d
Vinci-217 <1341060890@qq.com>
2024-09-25 10:25:24 +0800

    feat: 21

---
+++ 21.java
@@ -0,0 +1,47 @@
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode() {}
+ * ListNode(int val) { this.val = val; }
+ * ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+// 递归法
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        if(list1 == null)
+            return list2;
+        if(list2==null)
+            return list1;
+        if(list1.val<list2.val){
+            list1.next = mergeTwoLists(list1.next,list2);
+            return list1;
+        }
+        list2.next = mergeTwoLists(list1,list2.next);
+        return list2;
+
+    }
+}
+// 迭代法
+class Solution {
+    public ListNode mergeTwoLists(ListNode list1, ListNode list2) {
+        ListNode dummy = new ListNode();
+        ListNode cur = dummy;
+        while(list1!=null&&list2!=null){
+            if(list1.val<list2.val){
+                cur.next = list1;
+                list1 = list1.next;
+            }
+            else{
+                cur.next = list2;
+                list2 = list2.next;
+            }
+            cur = cur.next;
+        }
+        cur.next = list1!=null?list1:list2;
+        return dummy.next;
+
+    }
+}
