736ae7c3dd6e650f365f9baab5211a4dd1929887
Vinci-217 <1341060890@qq.com>
2024-09-26 08:22:57 +0800

    refactor: 232

--- 232.java
+++ 232.java
@@ -43,3 +43,60 @@ class MyQueue {
  * int param_3 = obj.peek();
  * boolean param_4 = obj.empty();
  */
+
+
+ // 第二次自己做的版本
+ class MyQueue {
+
+    private Stack<Integer> stackA;
+    private Stack<Integer> stackB;
+
+
+    public MyQueue() {
+        stackA = new Stack<>();
+        stackB = new Stack<>();
+    }
+    
+    public void push(int x) {
+        stackA.push(x);
+    }
+    
+    public int pop() {
+        while(!stackA.isEmpty()){
+            stackB.push(stackA.peek());
+            stackA.pop();
+        }
+        int peek = stackB.pop();
+        while(!stackB.isEmpty()){
+            stackA.push(stackB.peek());
+            stackB.pop();
+        }
+        return peek;
+    }
+    
+    public int peek() {
+        while(!stackA.isEmpty()){
+            stackB.push(stackA.peek());
+            stackA.pop();
+        }
+        int peek = stackB.peek();
+        while(!stackB.isEmpty()){
+            stackA.push(stackB.peek());
+            stackB.pop();
+        }
+        return peek;
+    }
+    
+    public boolean empty() {
+        return stackA.isEmpty() && stackB.isEmpty();
+    }
+}
+
+/**
+ * Your MyQueue object will be instantiated and called as such:
+ * MyQueue obj = new MyQueue();
+ * obj.push(x);
+ * int param_2 = obj.pop();
+ * int param_3 = obj.peek();
+ * boolean param_4 = obj.empty();
+ */
