96b1e7e3131e1396357b762a9a1480b0d5565f46
Vinci-217 <1341060890@qq.com>
2024-09-26 11:28:14 +0800

    refactor: 234

--- 234.java
+++ 234.java
@@ -35,3 +35,27 @@ class Solution {
 
     }
 }
+
+// 字符串更加简洁的方法
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public boolean isPalindrome(ListNode head) {
+        StringBuilder sb = new StringBuilder();
+        while(head!=null){
+            sb.append(head.val);
+            head = head.next;
+        }
+        String s1 = sb.toString();
+        String s2 = sb.reverse().toString();
+        return s1.equals(s2);
+    }
+}
