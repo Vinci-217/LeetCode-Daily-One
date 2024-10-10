37cde9b78ab5bf3c69ac7976172777780f73b9ea
Vinci-217 <1341060890@qq.com>
2024-10-10 10:12:40 +0800

    feat: 1114

---
+++ 1114.java
@@ -0,0 +1,35 @@
+class Foo {
+
+    private AtomicInteger first = new AtomicInteger(0);
+    private AtomicInteger second = new AtomicInteger(0);
+
+    public Foo() {
+        
+    }
+
+    public void first(Runnable printFirst) throws InterruptedException {
+        
+        // printFirst.run() outputs "first". Do not change or remove this line.
+        printFirst.run();
+
+        first.incrementAndGet();
+    }
+
+    public void second(Runnable printSecond) throws InterruptedException {
+        while(first.get()!=1){
+            // 阻塞
+        }
+        // printSecond.run() outputs "second". Do not change or remove this line.
+        printSecond.run();
+        second.incrementAndGet();
+    }
+
+    public void third(Runnable printThird) throws InterruptedException {
+        while(second.get()!=1){
+
+        }
+        // printThird.run() outputs "third". Do not change or remove this line.
+        printThird.run();
+        second.incrementAndGet();
+    }
+}
