610dfffc7bf984d585aa2dc4a53a53661a8cb30d
Vinci-217 <1341060890@qq.com>
2024-10-30 14:04:23 +0800

    refactor: 141

--- 141.java
+++ 141.java
@@ -69,3 +69,31 @@ public class Solution {
         return false;
     }
 }
+
+// 第三次做法
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ * int val;
+ * ListNode next;
+ * ListNode(int x) {
+ * val = x;
+ * next = null;
+ * }
+ * }
+ */
+public class Solution {
+    public boolean hasCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = slow;
+        while(slow!=null&&fast!=null&&fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow==fast){
+              return true;  
+            }
+        }
+        return false;
+
+    }
+}
