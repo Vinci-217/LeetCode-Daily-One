e610953938b63dde301dd049be80a628fba6f7d7
Vinci-217 <1341060890@qq.com>
2024-10-30 14:06:30 +0800

    refactor: 142

--- 142.java
+++ 142.java
@@ -62,3 +62,35 @@ public class Solution {
         
     }
 }
+
+// 第三次
+/**
+ * Definition for singly-linked list.
+ * class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode(int x) {
+ *         val = x;
+ *         next = null;
+ *     }
+ * }
+ */
+public class Solution {
+    public ListNode detectCycle(ListNode head) {
+        ListNode slow = head;
+        ListNode fast = slow;
+        while(slow!=null&&fast!=null&&fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+            if(slow==fast){
+              while(head!=slow){
+                slow = slow.next;
+                head = head.next;
+              }
+              return slow;
+            }
+        }
+        return null;
+
+    }
+}
