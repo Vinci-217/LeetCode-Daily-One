b7f49dcaf07031dd78e3e40eb7929e282dcdc4ae
Vinci <1341060890@qq.com>
2024-11-11 21:45:59 +0800

    refactor: 143

--- 143.java
+++ 143.java
@@ -46,3 +46,57 @@ class Solution {
     }
 
 }
+
+// 第二遍自己做出来了
+/**
+ * Definition for singly-linked list.
+ * public class ListNode {
+ *     int val;
+ *     ListNode next;
+ *     ListNode() {}
+ *     ListNode(int val) { this.val = val; }
+ *     ListNode(int val, ListNode next) { this.val = val; this.next = next; }
+ * }
+ */
+class Solution {
+    public void reorderList(ListNode head) {
+        ListNode mid = midNode(head);
+        ListNode head2 = reverse(mid);
+        while(head2.next!=null){
+            ListNode nxt = head.next;
+            ListNode nxt2 = head2.next;
+            head.next = head2;
+            head2.next = nxt;
+            head = nxt;
+            head2 = nxt2;
+        }
+
+    }
+
+
+    private ListNode midNode(ListNode head){
+        ListNode slow = head;
+        ListNode fast = head;
+        while(fast!=null&&fast.next!=null){
+            slow = slow.next;
+            fast = fast.next.next;
+        }
+        return slow;
+    }
+
+
+    private ListNode reverse(ListNode head){
+        ListNode pre = null;
+        ListNode cur = head;
+        while(cur!=null){
+            ListNode nxt = cur.next;
+            cur.next = pre;
+            pre = cur;
+            cur = nxt;
+        }
+        return pre;
+    }
+
+
+
+}
