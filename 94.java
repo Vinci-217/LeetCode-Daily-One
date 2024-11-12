230b10df6cae2b1a02204af660ddcdb0a101f3b9
Vinci-217 <1341060890@qq.com>
2024-11-12 22:56:37 +0800

    feat: refactor: 94

--- 94.java
+++ 94.java
@@ -36,3 +36,56 @@ class Solution {
     }
 
 }
+
+// 一个更好的解决方法
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+
+    class Node{
+        TreeNode node;
+        int color;
+        public Node(TreeNode node,int color){
+            this.node = node;
+            this.color = color;
+        }
+    }
+
+
+    List<Integer> ans = new ArrayList<>();
+    public List<Integer> inorderTraversal(TreeNode root) {
+        List<Integer> ans = new ArrayList<>();
+        Deque<Node> stack = new LinkedList<>();
+        if(root==null)
+            return ans;
+        stack.push(new Node(root,1));
+        while(!stack.isEmpty()){
+            Node cur = stack.pop();
+            if(cur.node ==null){
+                continue;
+            }
+            if(cur.color ==1){
+                stack.push(new Node(cur.node.right,1));
+                stack.push(new Node(cur.node,2));
+                stack.push(new Node(cur.node.left,1));
+            }
+            else{
+                ans.add(cur.node.val);
+            }
+        }
+        return ans;
+    }
+}
