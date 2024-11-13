325fe1c7d2388bce5428f7b0d7ad97f881c49e6c
Vinci <1341060890@qq.com>
2024-11-13 20:45:54 +0800

    feat: 230

--- 230.java
+++ 230.java
@@ -30,3 +30,40 @@ class Solution {
         return root.val;
     }
 }
+
+// 第二次做法，中序遍历以后寻找需要的点
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private List<Integer> list = new ArrayList<>();
+    public int kthSmallest(TreeNode root, int k) {
+        if(root==null){
+            return 0;
+        }
+        dfs(root);
+        return list.get(k-1);
+    }
+
+    private void dfs(TreeNode root){
+        if(root==null)
+            return;
+        dfs(root.left);
+        list.add(root.val);
+        dfs(root.right);
+
+    }
+
+}
