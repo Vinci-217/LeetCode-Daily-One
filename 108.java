fa64da9742d567c8fcc06b15b1d66d41cd6885c8
Vinci <1341060890@qq.com>
2024-11-13 17:17:12 +0800

    refactor: 108

--- 108.java
+++ 108.java
@@ -30,3 +30,35 @@ class Solution {
         return root;
     } 
 }
+
+
+// 第二次做法
+
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    public TreeNode sortedArrayToBST(int[] nums) {
+        return dfs(nums,0,nums.length-1);
+    }
+
+    private TreeNode dfs(int[] nums,int left,int right){
+        if(left > right){
+            return null;
+        }
+        int m = left + (right-left)/2;
+        return new TreeNode(nums[m],dfs(nums,left,m-1),dfs(nums,m+1,right));
+    }
+}
