0161e434bb806d5c1f25e6bfc12043c761b822e4
Vinci <1341060890@qq.com>
2024-11-13 21:07:09 +0800

    refactor: 114

--- 114.java
+++ 114.java
@@ -37,3 +37,40 @@ class Solution {
 
     }
 }
+
+// 第二次做法，递归后循环遍历更改
+/**
+ * Definition for a binary tree node.
+ * public class TreeNode {
+ *     int val;
+ *     TreeNode left;
+ *     TreeNode right;
+ *     TreeNode() {}
+ *     TreeNode(int val) { this.val = val; }
+ *     TreeNode(int val, TreeNode left, TreeNode right) {
+ *         this.val = val;
+ *         this.left = left;
+ *         this.right = right;
+ *     }
+ * }
+ */
+class Solution {
+    private List<TreeNode> list = new ArrayList<>();
+    public void flatten(TreeNode root) {
+        front(root);
+        for(int i = 1;i<list.size();i++){
+            TreeNode pre = list.get(i-1);
+            TreeNode cur = list.get(i);
+            pre.left = null;
+            pre.right = cur;
+        }
+    }
+
+    private void front(TreeNode node){
+        if(node==null)
+            return;
+        list.add(node);
+        front(node.left);
+        front(node.right);
+    }
+}
