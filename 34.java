021624f13f33dd2d1d2649ffcf481ed2a2fd09f9
Vinci <1341060890@qq.com>
2024-11-14 20:08:48 +0800

    refactor: 34

--- 34.java
+++ 34.java
@@ -23,3 +23,31 @@ class Solution {
 
     }
 }
+
+// 第二次做法
+class Solution {
+    public int[] searchRange(int[] nums, int target) {
+        
+        int n1 = search(nums,target);
+        if(n1==nums.length||nums[n1]!=target){
+            return new int[]{-1,-1};
+        }
+        int n2 = search(nums,target+1);
+        return new int[]{n1,n2-1};
+    }
+
+    private int search(int[] nums,int target){
+        int left = 0;
+        int right = nums.length-1;
+        while(left<=right){
+            int mid = left+(right-left)/2;
+            if(nums[mid]<target){
+                left = mid+1;
+            }
+            else{
+                right = mid-1;
+            }
+        }
+        return left;
+    }
+}
