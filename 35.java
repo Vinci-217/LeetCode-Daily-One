4100d2965744722c1be5813f995ba302c417431b
Vinci <1341060890@qq.com>
2024-11-14 09:24:16 +0800

    refactor: 35

--- 35.java
+++ 35.java
@@ -16,3 +16,21 @@ class Solution {
 
 
 }
+
+// 第二次做法，彻底搞懂红蓝染色和开闭区间
+class Solution {
+    public int searchInsert(int[] nums, int target) {
+        int left = 0;
+        int right = nums.length-1;
+        while(left<=right){
+            int mid = left + (right-left)/2;
+            if(nums[mid]<target){
+                left = mid+1;
+            }
+            else{
+                right = mid-1;
+            }
+        }
+        return left;
+    }
+}
