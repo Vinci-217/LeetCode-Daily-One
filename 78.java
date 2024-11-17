4d5f94a9511cb40d1450a026172deb29b85bf1bf
Vinci-217 <1341060890@qq.com>
2024-11-17 22:39:14 +0800

    refactor: 78

--- 78.java
+++ 78.java
@@ -22,3 +22,56 @@ class Solution {
         path.remove(path.size()-1);
     }
 }
+
+
+// 第二种做法：选或不选
+class Solution {
+
+    private List<List<Integer>> ans = new ArrayList<>();
+    private List<Integer> path = new ArrayList<>();
+    private int[] nums;
+    public List<List<Integer>> subsets(int[] nums) {
+        this.nums = nums;
+        dfs(0);
+        return ans;
+    }
+
+    private void dfs(int length){
+        if(length==nums.length){
+            ans.add(new ArrayList<>(path));
+            return;
+        }
+
+        length++;
+        dfs(length);
+
+        path.add(nums[length-1]);
+        dfs(length);
+        path.remove(path.size()-1);
+    }
+}
+
+
+// 第三种做法：获取的是路径而不是叶子节点，所以其实通过i控制了待选择的元素
+class Solution {
+
+    private List<List<Integer>> ans = new ArrayList<>();
+    private List<Integer> path = new ArrayList<>();
+    private int[] nums;
+    public List<List<Integer>> subsets(int[] nums) {
+        this.nums = nums;
+        dfs(0);
+        return ans;
+    }
+
+    private void dfs(int length){
+
+        ans.add(new ArrayList<>(path));
+
+        for(int i = length;i<nums.length;i++){
+            path.add(nums[i]);
+            dfs(i+1);
+            path.remove(path.size()-1);
+        }
+    }
+}
