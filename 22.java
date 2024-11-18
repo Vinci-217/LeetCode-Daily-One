d6ed69a1d7e39408a15e1131f704a504ad2381cb
Vinci <1341060890@qq.com>
2024-11-18 11:26:58 +0800

    refactor: 22

--- 22.java
+++ 22.java
@@ -30,3 +30,46 @@ class Solution {
         }
     }
 }
+
+// 第二次自己的做法
+class Solution {
+    private List<String> ans = new ArrayList<>();
+    private StringBuilder path = new StringBuilder();
+    private int n;
+
+    public List<String> generateParenthesis(int n) {
+        this.n = n;
+        dfs(0, 0);
+        return ans;
+    }
+
+    private void dfs(int l, int r) {
+        if (l == n && r == n) {
+            ans.add(new String(path));
+            return;
+        }
+
+        if (l == 0) {
+            path.append("(");
+            l++;
+        }
+
+        if (l < n) {
+
+            path.append("(");
+            l++;
+            dfs(l, r);
+            l--;
+            path.setLength(path.length()-1);
+        }
+        if (r < n&&l>r) {
+
+            path.append(")");
+            r++;
+            dfs(l, r);
+            r--;
+            path.setLength(path.length()-1);
+        }
+
+    }
+}
