fa26475794e69dc9e2c525ad3ca6b2314ec0f2cd
Vinci <1341060890@qq.com>
2024-11-19 11:16:51 +0800

    refactor: 131

--- 131.java
+++ 131.java
@@ -42,3 +42,50 @@ class Solution {
         return true;
     }
 }
+
+
+// 第二次做法，注意最后一个选择时，最后一个逗号是必选的，所以是s.length()-1
+class Solution {
+    private List<String> path = new ArrayList<>();
+    private List<List<String>> ans = new ArrayList<>();
+    private String s; 
+    public List<List<String>> partition(String s) {
+        this.s = s;
+        dfs(0,0);
+        return ans;
+    }
+
+
+    private void dfs(int length,int start){
+        if(length == s.length()){
+            ans.add(new ArrayList<>(path));
+            return;
+        }
+
+        if(length<s.length()-1){
+            dfs(length+1,start);
+        }
+
+        if(isOk(s.substring(start,length+1))){
+            path.add(s.substring(start,length+1));
+            dfs(length+1,length+1);
+            path.remove(path.size()-1);
+        }
+
+
+    }
+
+
+    private boolean isOk(String str){
+        int left = 0;
+        int right = str.length()-1;
+        while(left<right){
+            if(str.charAt(left)!=str.charAt(right)){
+                return false;
+            }
+            left++;
+            right--;
+        }
+        return true;
+    }
+}
