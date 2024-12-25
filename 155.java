b05e841a5d27bde6c4f2a8e37543b4a1f9408531
fanbotao02 <fanbotao02@meituan.com>
2024-12-25 17:35:06 +0800

    refactor: 155

--- 155.java
+++ 155.java
@@ -30,6 +30,43 @@ class MinStack {
     }
 }
 
+// 第二次做法
+/**
+ * Your MinStack object will be instantiated and called as such:
+ * MinStack obj = new MinStack();
+ * obj.push(val);
+ * obj.pop();
+ * int param_3 = obj.top();
+ * int param_4 = obj.getMin();
+ */
+
+
+ class MinStack {
+
+    // 每次寸一个数组，0号位置表示当前元素，1号位置表示最小元素
+    private final Deque<int[]> stack = new ArrayDeque<>();
+
+    public MinStack() {
+        stack.push(new int[]{0,Integer.MAX_VALUE});
+    }
+    
+    public void push(int val) {
+        stack.push(new int[]{val,Math.min(getMin(),val)});
+    }
+    
+    public void pop() {
+        stack.pop();
+    }
+    
+    public int top() {
+        return stack.peek()[0];
+    }
+    
+    public int getMin() {
+        return stack.peek()[1];
+    }
+}
+
 /**
  * Your MinStack object will be instantiated and called as such:
  * MinStack obj = new MinStack();
